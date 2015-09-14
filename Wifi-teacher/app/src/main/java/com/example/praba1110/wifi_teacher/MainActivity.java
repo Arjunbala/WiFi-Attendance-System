bf6bec655c8c26d516cbb53f4f295dcd1aeaa37c
praba1110 <praba1110@gmail.com>
2015-09-14 20:59:41 +0530

    Added both student and teacher app layout

---
+++ Wifi-teacher/app/src/main/java/com/example/praba1110/wifi_teacher/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.praba1110.wifi_teacher;
+
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView time;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        time= (TextView) findViewById(R.id.textView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    public void attendance(View v){
+        final discovery d=new discovery();
+        d.start_discovery();
+        new CountDownTimer(10000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                time.setText(String.valueOf(millisUntilFinished/1000));
+            }
+
+            @Override
+            public void onFinish() {
+                Toast.makeText(getApplicationContext(),"Attendance taken",Toast.LENGTH_SHORT).show();
+                d.end_discovery();
+            }
+        }.start();
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ Wifi-teacher/app/src/main/java/com/example/praba1110/wifi_teacher/discovery.java
@@ -0,0 +1,19 @@
+package com.example.praba1110.wifi_teacher;
+
+/**
+ * Created by praba1110 on 14/9/15.
+ */
+public class discovery {
+    String[] devices=new String[100]; //list of roll numbers
+    int number = 0; //number of responses
+    boolean dicovery_on = true;
+    public void start_discovery() {
+
+    }
+    public void end_discovery() {
+
+    }
+    public String[] getDevices() {
+        return devices;
+    }
+}
